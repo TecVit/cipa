a7983468f16f9c5be6f156dd5908ee64d2c34d30
TecVit <vitorfreelancer021@gmail.com>
2024-09-29 17:31:08 -0300

    atualizando relatar, incrementando 1 relatoria nas estatisticas

--- src/firebase/relatar.js
+++ src/firebase/relatar.js
@@ -1,6 +1,7 @@
 import { clearCookies, deleteCookie, getCookie, setCookie } from "./cookies";
 import { firestore, auth } from "./logar";
 import { v4 as uuidv4 } from 'uuid';
+import { increment } from "firebase/firestore"; 
 
 // Dados
 const uidCookie = getCookie('uid') || '';
@@ -10,6 +11,14 @@ const emailCookie = getCookie('email') || '';
 const date = new Date();
 let ano = date.getFullYear(); 
 
+function formatDateToDDMMYYYY(dateObject) {
+    const day = String(dateObject.getDate()).padStart(2, '0');    // Dia com dois dígitos
+    const month = String(dateObject.getMonth() + 1).padStart(2, '0'); // Mês com dois dígitos (+1 porque getMonth() retorna de 0 a 11)
+    const year = dateObject.getFullYear();  // Ano com 4 dígitos
+
+    return `${day}-${month}-${year}`;
+}
+
 // Funções
 const createReport = async (dados) => {
     const { nome, email, motivo, descricao } = dados;
@@ -18,6 +27,12 @@ const createReport = async (dados) => {
         return false;
     }
     try {
+        const formattedDate = await formatDateToDDMMYYYY(date);
+        const reportDayRef = firestore.collection(`estatisticas-${ano}`).doc('relatos')
+        await reportDayRef.set({
+            [formattedDate]: increment(1),
+        }, { merge: true });
+        
         const reportDoc = await firestore.collection(`relatos-${ano}`)
         .doc(uuid).set({
             nome,
